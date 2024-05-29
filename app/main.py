17b4bda42b817a17d798702ad88304c21fe26f5c
Michael Corry <corryvert@gmail.com>
2024-05-29 09:55:21 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
 import socket
+import os
+import time
 
-
+def handle_request(connection):
+    try:
+        while True:
+            request = connection.recv(1024)
+            if not request:
+                # Break the loop if the client disconnects
+                break
+            print(request.decode())
+            response = "+PONG\r\n"
+            connection.sendall(response.encode())
+    except Exception as e:
+        print(f"Error handling: {e}")
+    finally:
+        connection.close()
+    
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    response = "+PONG\r\n"
-    connection, address = server_socket.accept() # wait for client
     
     while True:
-        data = connection.recv(1024)
-        print(data.decode())
-        
-        connection.sendall(response.encode())
-        #connection.close()
-        continue
-
+        connection, address = server_socket.accept() # wait for client
+        pid = os.fork()
+        if pid == 0: #child
+            server_socket.close() #close child copy
+            handle_request(connection)
+            print(f"Connection from {address} closed")
+            os._exit(0)# child exits here
+        else:
+            connection.close() # parent closes its copy of the connection
 
 if __name__ == "__main__":
     main()
